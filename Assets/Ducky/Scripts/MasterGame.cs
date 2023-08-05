8231f2afe844f3a239e48a9d82a98ba7d35441da
Duckint <music80001@gmail.com>
2023-08-05 19:29:43 -0400

    smoothed opening to gameplay
    pretty clever trick with the time.deltaTime actually

--- Assets/Ducky/Scripts/MasterGame.cs
+++ Assets/Ducky/Scripts/MasterGame.cs
@@ -8,18 +8,25 @@ public class MasterGame : MonoBehaviour
     public bool dontPlaycutscene = false;
     bool cutscene1 = true;
     public bool cutscene2 = false;
+    private double timepassed = 0;
 
     public void Cutscenes()
     {
         if (cutscene1)
         {
             //false is true, true is false dont ask it just is
-            dontPlaycutscene = false;
+            GameObject arms = GameObject.FindGameObjectWithTag("Cutscene Model");
+            timepassed += Time.deltaTime;
             GameObject CameraFollow;
             CameraFollow = GameObject.Find("AcameraBone");
-            transform.position = new Vector3(CameraFollow.transform.position.x + 0.01f, CameraFollow.transform.position.y + 0.1f,
-                CameraFollow.transform.position.z - 0.3f);
+            transform.position = new Vector3(CameraFollow.transform.position.x + 0.01f, CameraFollow.transform.position.y + 0.1f,CameraFollow.transform.position.z - 0.3f);
             transform.rotation = CameraFollow.transform.rotation;
+            if (timepassed >= 20)
+            {
+                dontPlaycutscene = true;
+                Destroy(arms);
+                
+            }
         }
         if (cutscene2)
         {
