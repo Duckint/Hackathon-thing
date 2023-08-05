00f138dbc512f244bd51e2b63a584eef7cb351f5
Duckint <music80001@gmail.com>
2023-08-04 23:16:34 -0400

    arms
    woahhhhhhhhhhohohohohohohohohohohohhhhhhooooooooooooooooo woahahohohohoooooooooooooo

--- Assets/Ducky/Scripts/Player/Abilities.cs
+++ Assets/Ducky/Scripts/Player/Abilities.cs
@@ -5,14 +5,32 @@ using UnityEngine;
 
 public class Abilities : MonoBehaviour
 {
+    private GameObject fun2;
+    private Animator fun;
+    private void Start()
+    {
+        fun2 = GameObject.Find("Arms");
+        fun = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.K))
+        {
+            fun2.GetComponent<Animator>().SetTrigger("punchtrig");
+        }
+    }
     private void OnCollisionStay(Collision collide)
     {
+
         if (collide.gameObject.tag == "Enemy")
         {
             if (Input.GetMouseButton(0))
             {
                 Destroy(collide.gameObject);
             }
+            
         } 
     }
+    
 }
